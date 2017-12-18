bc849901739f023f58eea1d953068c021303b6b6
slyty7 <tyler.j.west.7@gmail.com>
2017-12-18 11:35:42 -0800

    add errors to front end

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -13,6 +13,8 @@
       v-model="password"
       placeholder="password" />
     <br>
+    <div class="error" v-html="error" />
+    <br>
     <button @click="register">Register</button>
 
   </div>
@@ -24,21 +26,27 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: ''
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.error {
+  color: red
+}
 </style>
--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -12,7 +12,7 @@ module.exports = {
     const {error, value} = Joi.validate(req.body, schema)
     
     if (error) {
-      switch (error.deatils[0].context.key) {
+      switch (error.details[0].context.key) {
         case 'email':
           res.status(400).send({
           	error: 'You must provide a valid email address.'
